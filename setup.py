f415cb91c05170458b6aa2ef3631945c114b4f48
Fernando Corrales <corrales_fernando@hotmail.com>
2022-11-19 23:32:10 -0300

    feat: add metadata to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='apys',
+    description='Set of python functions to access different APIs',
+    author='Fernando Corrales',
+    author_email='corrales_fernando@hotmail.com',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
 )
