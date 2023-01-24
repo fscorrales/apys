526a7d0e882b8875d6413b66d4defc5ffece947e
Fernando Corrales <corrales_fernando@hotmail.com>
2023-01-24 19:46:00 -0300

    feat: add my_homebroker and symbol_daily.py inside of it

---
+++ src/apys/my_homebroker/symbol_daily.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+"""
+Author: Fernando Corrales <fscorrales@gmail.com>
+Source: https://github.com/crapher/pyhomebroker
+Purpose: Get daily symbol data from HomeBroker
+Require package: 
+    -   pip install pyhomebroker
+    -   pip install pyhomebroker --upgrade --no-cache-dir
+"""
+
+import argparse
+import datetime as dt
+import inspect
+import json
+import os
+import sys
+from dataclasses import dataclass, field
+
+import pandas as pd
+from pyhomebroker import HomeBroker
+from ..utils.validation import valid_date
+from ..utils.pydyverse import PrintTibble
+
+
+# --------------------------------------------------
+@dataclass
+class SymbolDaily:
+    """
+    Get daily symbol data from HomeBroker
+    :param hb must be initialized first
+    """
+    hb: HomeBroker
+    symbol: str
+    from_date: dt.date
+    to_date: dt.date = dt.date.today()
+    df: pd.DataFrame = field(init=False, repr=False)
+
+    def __post_init__(self):
+        self.get_data()
+
+    def get_data(self):
+
+        # from_date = dt.datetime.strftime(self.from_date, "%Y-%m-%d")
+        # to_date = dt.datetime.strftime(self.to_date, "%Y-%m-%d")
+
+        df = self.hb.history.get_daily_history(
+            self.symbol, self.from_date, self.to_date)
+        self.df = pd.DataFrame(df)
+
+    def print_tibble(self):
+        print(PrintTibble(self.df))
+
+# --------------------------------------------------
+def get_args():
+    """Get needed params from user input"""
+    parser = argparse.ArgumentParser(
+        description = 'Get daily symbol data from HomeBroker',
+        formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        'symbol', 
+        metavar = 'Symbol',
+        type=str,
+        help = "Symbol to look up")
+
+    parser.add_argument(
+        'from_date', 
+        metavar = 'from_date',
+        # type=valid_date,
+        help = "The Start Date - format DD-MM-YYYY")
+
+    parser.add_argument(
+        '-to', '--to_date', 
+        metavar = 'to_date',
+        default = dt.datetime.strftime(dt.date.today(), "%d-%m-%Y"),
+        # type=valid_date,
+        help = "The End Date - format DD-MM-YYYY")
+
+    parser.add_argument(
+        '-i', '--id_broker', 
+        metavar = 'id_broker',
+        default = '',
+        type=str,
+        help = "id_broker to log with HomeBroker")
+
+    parser.add_argument(
+        '-d', '--dni', 
+        metavar = 'dni',
+        default = '',
+        type=str,
+        help = "dni to log in HomeBroker")
+
+    parser.add_argument(
+        '-u', '--user', 
+        metavar = 'User',
+        default = '',
+        type=str,
+        help = "User to log in HomeBroker")
+
+    parser.add_argument(
+        '-p', '--password', 
+        metavar = 'Password',
+        default = '',
+        type=str,
+        help = "Password to log in HomeBroker")
+
+    return parser.parse_args()
+
+# --------------------------------------------------
+def main():
+    """Let's try it"""
+    args = get_args()
+    dir_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    json_path = dir_path + '/cocos.json'
+    
+    if args.id_broker != '' and args.user != '' and args.password != '' and args.dni != '':
+        hb = HomeBroker(int(args.id_broker))
+        hb.auth.login(
+            dni = args.dni, user = args.user, 
+            password = args.password, raise_exception=True
+            )
+    else:
+        if os.path.isfile(json_path):
+            with open(json_path) as json_file:
+                data_json = json.load(json_file)
+                hb = HomeBroker(int(data_json['broker']))
+                hb.auth.login(
+                    dni = data_json['dni'], user = data_json['user'], 
+                    password = data_json['password'], raise_exception=True
+                    )
+            json_file.close()
+        else:
+            msg = (
+                f'If {json_path} with username and password ' +
+                'as keys does not exist in the directory, ' + 
+                'both arguments must be given.'
+            )
+            sys.exit(msg)
+
+    if isinstance(args.from_date, str):
+        from_date = dt.datetime.strptime(args.from_date, '%d-%m-%Y').date()
+    if isinstance(args.to_date, str):
+        to_date = dt.datetime.strptime(args.to_date, '%d-%m-%Y').date()
+
+    test = SymbolDaily(
+        hb = hb,
+        symbol = args.symbol,
+        from_date = from_date,
+        to_date = to_date,
+    )
+    test.print_tibble()
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
+    # From apys.src
+    # python -m apys.my_homebroker.symbol_daily GGAL 01-10-2022
