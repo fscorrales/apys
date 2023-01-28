af811a470bcc5be70d1444cac65b339754551e33
Fernando Corrales <corrales_fernando@hotmail.com>
2023-01-28 12:54:55 -0300

    feat: add to_sql method to IOL's asset_class_country.py

--- src/apys/iol/asset_class_country.py
+++ src/apys/iol/asset_class_country.py
@@ -16,24 +16,31 @@ import requests
 from datar import dplyr, f
 
 from ..utils.pydyverse import PrintTibble
-from ..db.from_to_sql import FromToSQL
+from ..utils.sql_utils import SQLUtils
 from .connect import IOL
-from ..db.connect import ConnectDB
-
+from ..models.iol_model import IOLModel
 
 # --------------------------------------------------
 @dataclass
-class InstrumentsCountry(FromToSQL):
+class AssetClassCountry(SQLUtils):
     """
     Get available instruments by country from IOL
     :param IOL must be initialized first
     """
     iol: IOL
-    country: str
+    country: str #argentina o estados_Unidos
     response: requests.Response = field(init=False, repr=False)
     df: pd.DataFrame = field(init=False, repr=False)
+    _TABLE_NAME:str = field(init=False, repr=False, default='iol_asset_class_country')
+    _INDEX_COL:str = field(init=False, repr=False, default='')
+    _FILTER_COL:str = field(init=False, repr=False, default='country')
+    _SQL_MODEL:IOLModel = field(init=False, repr=False, default=IOLModel)
 
-    def download_data(self):
+    def __post_init__(self):
+        self.get_data()
+        self.to_dataframe()
+
+    def get_data(self):
         """Get response from IOL"""
         self.iol.update_token()
         h = {
@@ -53,17 +60,12 @@ class InstrumentsCountry(FromToSQL):
 
         df = df >> \
             dplyr.transmute(
-                instrument = f.instrumento,
+                asset_class = f.instrumento,
                 country = f.pais
             )
 
         self.df = (df) 
         return self.df
-
-    def to_sql(self):
-        FromToSQL.to_sql(
-            self.df, 'asset_class_country'
-        )
 
     def print_tibble(self):
         print(PrintTibble(self.df))
@@ -133,14 +135,22 @@ def main():
             )
             sys.exit(msg)
 
-    sql_path = dir_path + '/iol.sqlite'
-    with ConnectDB(sql_path) as con:
-        test = InstrumentsCountry(
-            iol = iol,
-            country = args.country,
-            engine = con.engine
-        )
-        test.print_tibble()
+    # sql_path = dir_path + '/iol.sqlite'
+    # with ConnectDB(sql_path) as con:
+    #     test = InstrumentsCountry(
+    #         iol = iol,
+    #         country = args.country,
+    #         engine = con.engine
+    #     )
+    #     test.print_tibble()
+
+    test = AssetClassCountry(
+        iol = iol,
+        country = args.country
+    )
+    test.print_tibble()
+    test.to_sql(dir_path + '/arg_market.sqlite')
+
 
     # json_file created with credentials
     if args.json_file:
@@ -158,10 +168,10 @@ def main():
 if __name__ == '__main__':
     main()
     # From apys.src
-    # python -m apys.iol.instruments_country argentina -j True
+    # python -m apys.iol.asset_class_country argentina -j True
     
     # A tidy dataframe: 6 X 2
-    #   instrument    country
+    #   asset_class   country
     #     <object>   <object>
     # 0   Acciones  argentina
     # 1      Bonos  argentina
